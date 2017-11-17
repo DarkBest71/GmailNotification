5812c44b3185c98efaed3b3e1503188121d93897
seb03000 <avatardu33@gmail.com>
2017-11-17 16:52:42 +0100

    Add files via upload

---
+++ GmailNotification/GmailNotification.js
@@ -0,0 +1,62 @@
+exports.action = function(data, callback, config){
+console.log('##### GmailNotification #####');
+var config = config.modules.GmailNotification;	
+switch (data.request)
+{
+case 'compteunread':
+get_mail(compte, data, callback, config );
+break;
+case 'readmails':
+get_mail(read, data, callback, config );
+break;
+default:
+output(callback, "Une erreur s'est produite: ");
+}
+}
+
+var get_mail = function (action, data, callback, config ) {
+console.log("***** connection *****");
+var https = require('https'),
+xml2js = require('xml2js');
+var parser = new xml2js.Parser({trim: true});
+var options = {
+host: "mail.google.com",
+path: "/mail/feed/atom",
+port: 443,
+method: "GET",
+auth: config.user + ":" + config.password
+};
+
+https.get(options, function(res) {
+res.setEncoding('utf8');
+var fullResponse = "";
+res.on('data', function (chunk) {
+fullResponse = fullResponse+chunk;
+});
+			
+res.on('end', function(){
+parser.parseString(fullResponse, function (err, result) {
+action(result, data, callback, config);
+});
+});
+}).on('error', function(e) {
+output(callback, "Une erreur s'est produite: " + e.message);
+});
+}
+
+var compte = function (result, data, callback, config){
+output(callback, "vous avez " + result.feed.fullcount + " emails non lus");
+}
+var read = function (result, data, callback, config){
+var parle = "";
+for ( var i = 0; i < result.feed.entry.length; i++ ) {
+var mails = result.feed.entry[i];
+console.log(mails);
+parle += " mail numero " + (i + 1) + " titre " + mails.title;
+}
+output (callback, parle);	
+}
+var output = function ( callback, output ) {
+console.log(output);
+callback({ 'tts' : output});
+}
---
+++ GmailNotification/README.md
@@ -0,0 +1,23 @@
+#GmailNotification fonctionne pour ce qui on un compte Gmail.
+	
+Plugin GmailNotification vous permet que des vous recevez un mail , et que vous demandez a sarah si vous avez un mail, elle vous le dira.
+
+En gros vous lui dite: Sarah verifie mes mails    elle vous répondra:  vous avez 1 emails non lus.
+
+Et si vous lui dite:  Sarah lecture de mes mails  elle vous répondra:  mail numero 1 titre: Sébastien aime votre commantaire.
+	
+Pour que cela fonctionne aller dans le fichier GmailNotification.prop modifier la ligne 7 est 8.
+
+#Ligne 7:
+
+"user"	   : "Wili@gmail.com",
+Remplacer cette email par la votre.
+
+#Ligne 8:
+
+"password"   : "kitjt65446tgt68647477"
+Remplacer se code , part le mot de passe de votre compte Gmail.
+
+Pour savoir si cela fonctionne attendez de recevoir un mail, ou envoyez vous un mail, mail n'ouvrez pas votre mail pour le voir sinon cela marche pas
+
+
